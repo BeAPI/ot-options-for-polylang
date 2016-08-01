65efc2aca615a57052f425e35dd314c1c2091a90
Maxime CULEA <maxime.culea@yahoo.fr>
2016-08-01 17:27:50 +0200

    Add some comments

--- ot-options-for-polylang.php
+++ ot-options-for-polylang.php
@@ -15,16 +15,25 @@
 	 * @return mixed|string|void
 	 */
 	public static function change_ot_key_with_polylang( $key ) {
+		// Get Plylang current lang
 		$lang = pll_current_language();
 
 		if ( is_admin() ) {
+			// Get user lang meta
 			$lang = get_user_meta( get_current_user_id(), 'pll_filter_content', true );
+			
+			// Get the Polylang selected lang from admin
 			$selected_lang = filter_input( INPUT_GET, 'lang', FILTER_SANITIZE_STRING );
 			if ( ! empty( $selected_lang ) ) {
 				$lang = $selected_lang;
 			}
 		}
 
+		/**
+		 * Depending on if lang is empty or it is the default one
+		 * - return default wanted key
+		 * - change it to take the current lang : {$key}_{$lang}
+		 */
 		return empty( $lang ) || $lang === 'all' || $lang === pll_default_language() ? $key : $key . '_' . $lang;
 	}
 
